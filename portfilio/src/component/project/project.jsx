b0187e982e8205fd0a4bdafb5910949de8c84f22
Ali <aliyazdanpanahfard@gmail.com>
2025-10-27 14:39:02 +0330

    update

--- portfilio/src/component/project/project.jsx
+++ portfilio/src/component/project/project.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import "./project.css";
 
-const Prog = () => {
+
+const Prog = (props) => {
   return (
     <>
       <div className="ProgWraper">
@@ -17,10 +19,12 @@ const Prog = () => {
               <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
             </svg>
 
-            <p>Dynamic shop you can watch It in my Github</p>
+            <p> {props.Progs.shopText} </p>
 
             <div className="GitLink">
-              Open Github
+              <Link className="" to={"https://github.com/aliyazdanpanah-beep/Shoping-Cart"}>
+                {props.Progs.gitText}
+              </Link>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -49,9 +53,11 @@ const Prog = () => {
               <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486z" />
             </svg>
 
-            <p>Dynamic Todo list you can watch It in my Github</p>
+            <p> {props.Progs.listText} </p>
             <div className="GitLink">
-              Open Github
+              <Link className="" to={"https://github.com/aliyazdanpanah-beep/Shoping-Cart"}>
+                {props.Progs.gitText}
+              </Link>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -79,9 +85,11 @@ const Prog = () => {
               <path d="M13 .5c0-.276-.226-.506-.498-.465-1.703.257-2.94 2.012-3 8.462a.5.5 0 0 0 .498.5c.56.01 1 .13 1 1.003v5.5a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5zM4.25 0a.25.25 0 0 1 .25.25v5.122a.128.128 0 0 0 .256.006l.233-5.14A.25.25 0 0 1 5.24 0h.522a.25.25 0 0 1 .25.238l.233 5.14a.128.128 0 0 0 .256-.006V.25A.25.25 0 0 1 6.75 0h.29a.5.5 0 0 1 .498.458l.423 5.07a1.69 1.69 0 0 1-1.059 1.711l-.053.022a.92.92 0 0 0-.58.884L6.47 15a.971.971 0 1 1-1.942 0l.202-6.855a.92.92 0 0 0-.58-.884l-.053-.022a1.69 1.69 0 0 1-1.059-1.712L3.462.458A.5.5 0 0 1 3.96 0z" />
             </svg>
 
-            <p>Dynamic resturant you can watch Its in my GitHub</p>
+            <p> {props.Progs.restText} </p>
             <div className="GitLink">
-              Open Github
+              <Link className="" to={"https://github.com/aliyazdanpanah-beep/Shoping-Cart"}>
+                {props.Progs.gitText}
+              </Link>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
--- portfilio/src/page/home/home.jsx
+++ portfilio/src/page/home/home.jsx
@@ -1,16 +1,27 @@
 import Prog from "../../component/project/project";
 import { useState, useEffect } from "react";
+import axios from "axios";
 import "./home.css";
 
 const Home = () => {
   const [prog, setProg] = useState([])
+
+  useEffect(() => {
+    setProg([
+      axios.get("http://localhost:3000/prog").then((res) => {
+        setProg(res.data.data)
+      })
+    ])
+  }, [])
   return (
     <section className="container">
       <div className="hero">
         <h2>wellcom to Portfilio</h2>
       </div>
       <div className="prog">
-        <Prog />
+        {prog.map((Progs) => (
+          <Prog key={Progs.id} Progs={Progs} />
+        ))}
       </div>
     </section>
   );
