783b5131084e9e5b423eef6da2ae02ed3bd4415d
Ali <aliyazdanpanahfard@gmail.com>
2025-10-27 12:27:30 +0330

    update

--- portfilio/src/component/project/prohect.jsx
+++ portfilio/src/component/project/prohect.jsx
@@ -11,13 +11,29 @@ const Prog = () => {
               width="16"
               height="16"
               fill="currentColor"
-              class="icons"
+              className="icons"
               viewBox="0 0 16 16"
             >
               <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
             </svg>
+
+            <p>online shop with shopping cart you can watch It in my Github</p>
           </div>
-          <div className="ProgName"></div>
+
+          <div className="ProgName">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="icons"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486z" />
+            </svg>
+
+            <p>Dynamic Todo list you can watch It in my Github</p>
+          </div>
           <div className="ProgName"></div>
         </div>
       </div>
