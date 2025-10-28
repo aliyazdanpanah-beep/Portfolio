e089e6ef48e1f7d6653030b95c2c28c9a3927c9f
Ali <aliyazdanpanahfard@gmail.com>
2025-10-28 18:15:01 +0330

    update

---
+++ portfilio/src/component/skills/skill.jsx
@@ -0,0 +1,20 @@
+import 'skill.css'
+
+const Skills = () => {
+  return (
+    <>
+      <div className='SkillWrapper'>
+        <div className='Skills'>
+          <div className="SkillItem">html</div>
+          <div className="SkillItem">css</div>
+          <div className="SkillItem">JavaScript</div>
+          <div className="SkillItem">bootstrap</div>
+          <div className="SkillItem">tilwind</div>
+          <div className="SkillItem">react</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Skills
