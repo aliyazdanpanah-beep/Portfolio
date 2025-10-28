a6ae69889ab0e1cbdc86cbd4dbadc4de90a92e4a
Ali <aliyazdanpanahfard@gmail.com>
2025-10-28 19:19:13 +0330

    update

--- portfilio/src/component/skills/skill.jsx
+++ portfilio/src/component/skills/skill.jsx
@@ -1,15 +1,46 @@
+import { FaHtml5 } from 'react-icons/fa'
+import { FaCss3Alt } from "react-icons/fa";
+import { FaJsSquare } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { RiBootstrapLine } from "react-icons/ri";
+import { RiTailwindCssFill } from "react-icons/ri";
+
 import './skill.css'
 
 const Skills = () => {
   return (
     <div className="SkillWrapper">
       <div className="Skills">
-        <div className="SkillItem">html</div>
-        <div className="SkillItem">css</div>
-        <div className="SkillItem">JavaScript</div>
-        <div className="SkillItem">bootstrap</div>
-        <div className="SkillItem">tilwind</div>
-        <div className="SkillItem">react</div>
+
+        <div className="SkillItem"> 
+          <FaHtml5 className='Icons'/> 
+          <h3 className='IconTittle'>Html</h3>
+        </div>
+
+        <div className="SkillItem">
+          <FaCss3Alt className='Icons' />
+          <h3 className='IconTittle'>CSS</h3>
+        </div>
+
+        <div className="SkillItem">
+          <FaJsSquare className='Icons' />
+          <h3 className='IconTittle'>JavaScript</h3>
+        </div>
+
+        <div className="SkillItem">
+          <FaReact className='Icons' />
+          <h3 className='IconTittle'>React js</h3>
+        </div>
+
+        <div className="SkillItem">
+          <RiBootstrapLine className='Icons' />
+          <h3 className='IconTittle'>Bootstrap</h3>
+        </div>
+
+        <div className="SkillItem">
+          <RiTailwindCssFill className='Icons' />
+          <h3 className='IconTittle'>Tailwind</h3>
+        </div>
       </div>
     </div>
   );
