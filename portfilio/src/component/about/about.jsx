adc11e00ce118171f116f68866cb0dc0cfd77979
Ali <aliyazdanpanahfard@gmail.com>
2025-10-29 18:13:22 +0330

    update

--- portfilio/src/component/about/about.jsx
+++ portfilio/src/component/about/about.jsx
@@ -4,28 +4,30 @@ import './about.css'
 
 const About = () => {
   return (
-    <div className="AboutPage">
-      <div className="AboutContainer">
-        <div className="AboutSection">
-          <p className="AboutEmail">ali.yazdanpanahfard@gmail.com</p>
-          <p className="AboutRole">Frontend Developer</p>
-        </div>
-        
-        <div className="AboutSection">
-          <div className="ConnectWrapper">
-            <div className="SocialIcons">
-              <Link to="#" className="SocialLink"> 
-                <FaGithub className="Icon" /> 
-              </Link>
-              <Link to="#" className="SocialLink"> 
-                <FaLinkedin className="Icon" /> 
-              </Link>
+    <>
+      <div className="AboutPage">
+        <div className="AboutContainer">
+          <div className="AboutSection">
+            <p className="AboutEmail">ali.yazdanpanahfard@gmail.com</p>
+            <p className="AboutRole">Frontend Developer</p>
+          </div>
+
+          <div className="AboutSection">
+            <div className="ConnectWrapper">
+              <div className="SocialIcons">
+                <Link to="https://github.com/aliyazdanpanah-beep" className="SocialLink">
+                  <FaGithub className="Icon" />
+                </Link>
+                <Link to="https://www.linkedin.com/in/ali-yazdanpanah-79787a2a3/" className="SocialLink">
+                  <FaLinkedin className="Icon" />
+                </Link>
+              </div>
+              <p className="PhoneNumber">+98 993 100 1382</p>
             </div>
-            <p className="PhoneNumber">+98 993 100 1382</p>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
