e9dd942509ac1ac4e6d5e5549c334ff4c197a026
Ali <aliyazdanpanahfard@gmail.com>
2025-10-30 15:44:31 +0330

    update

---
+++ portfilio/src/page/about/AbotP.jsx
@@ -0,0 +1,18 @@
+import './AboutP.jsx'
+
+const AboutP = () => {
+  return (
+    <>
+      <section>
+        wellcom to my Portfolio web app 
+
+        <p>
+          actully in this project I eant show
+          my self and and my skills
+        </p>
+      </section>
+    </>
+  )
+}
+
+export default AboutP
