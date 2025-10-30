559a8915bbba70b87e4ead472c1e8df796e3d6c2
Ali <aliyazdanpanahfard@gmail.com>
2025-10-30 15:45:43 +0330

    update

--- portfilio/src/page/about/AbotP.jsx
+++ portfilio/src/page/about/AbotP.jsx
@@ -4,15 +4,126 @@ const AboutP = () => {
   return (
     <>
       <section>
-        wellcom to my Portfolio web app 
+        wellcom to my Portfolio web app
+        <p>actully in this project I eant show my self and and my skills</p>
+        <div className="Wrapper">
+          <div className="ProgWraper">
+            <div className="Progs">
+              <div className="ProgName">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="icons"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+                </svg>
 
-        <p>
-          actully in this project I eant show
-          my self and and my skills
-        </p>
+                <p>Dynamic shop you can watch It in my Github</p>
+
+                <div className="GitLink">
+                  <Link
+                    className=""
+                    to={"https://github.com/aliyazdanpanah-beep/Shoping-Cart"}
+                  >
+                    Github
+                  </Link>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="GitIcon"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
+                    />
+                  </svg>
+                </div>
+              </div>
+
+              <div className="ProgName">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="icons"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486z" />
+                </svg>
+
+                <p> Dynamic Todo list you can watch It in my Github </p>
+                <div className="GitLink">
+                  <Link
+                    className=""
+                    to={
+                      "https://github.com/aliyazdanpanah-beep/simpel_ToDoList"
+                    }
+                  >
+                    Github
+                  </Link>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="GitIcon"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="ProgName">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="icons"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M13 .5c0-.276-.226-.506-.498-.465-1.703.257-2.94 2.012-3 8.462a.5.5 0 0 0 .498.5c.56.01 1 .13 1 1.003v5.5a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5zM4.25 0a.25.25 0 0 1 .25.25v5.122a.128.128 0 0 0 .256.006l.233-5.14A.25.25 0 0 1 5.24 0h.522a.25.25 0 0 1 .25.238l.233 5.14a.128.128 0 0 0 .256-.006V.25A.25.25 0 0 1 6.75 0h.29a.5.5 0 0 1 .498.458l.423 5.07a1.69 1.69 0 0 1-1.059 1.711l-.053.022a.92.92 0 0 0-.58.884L6.47 15a.971.971 0 1 1-1.942 0l.202-6.855a.92.92 0 0 0-.58-.884l-.053-.022a1.69 1.69 0 0 1-1.059-1.712L3.462.458A.5.5 0 0 1 3.96 0z" />
+                </svg>
+
+                <p> Dynamic resturant you can watch It in my GitHub </p>
+                <div className="GitLink">
+                  <Link
+                    className=""
+                    to={"https://github.com/aliyazdanpanah-beep/Restorant"}
+                  >
+                    Github
+                  </Link>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="GitIcon"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
-  )
+  );
 }
 
 export default AboutP
