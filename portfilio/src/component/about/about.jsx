c009d180e9f8cd0e28b021c4d9b2a3f868f4c3b9
Ali <aliyazdanpanahfard@gmail.com>
2025-10-30 21:15:51 +0330

    Update about.jsx

--- portfilio/src/component/about/about.jsx
+++ portfilio/src/component/about/about.jsx
@@ -1,6 +1,7 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { Link } from 'react-router-dom'
 import './about.css'
+import DownloadButton from "../cvDownload/CV";
 
 const About = () => {
   return (
@@ -9,16 +10,22 @@ const About = () => {
         <div className="AboutContainer">
           <div className="AboutSection">
             <p className="AboutEmail">ali.yazdanpanahfard@gmail.com</p>
-            <p className="AboutRole">Frontend Developer</p>
+            <DownloadButton />
           </div>
 
           <div className="AboutSection">
             <div className="ConnectWrapper">
               <div className="SocialIcons">
-                <Link to="https://github.com/aliyazdanpanah-beep" className="SocialLink">
+                <Link
+                  to="https://github.com/aliyazdanpanah-beep"
+                  className="SocialLink"
+                >
                   <FaGithub className="Icon" />
                 </Link>
-                <Link to="https://www.linkedin.com/in/ali-yazdanpanah-79787a2a3/" className="SocialLink">
+                <Link
+                  to="https://www.linkedin.com/in/ali-yazdanpanah-79787a2a3/"
+                  className="SocialLink"
+                >
                   <FaLinkedin className="Icon" />
                 </Link>
               </div>
