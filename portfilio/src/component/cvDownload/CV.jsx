1c14e18ccca332aeaa596a8cdeb8871930b423d1
Ali <aliyazdanpanahfard@gmail.com>
2025-10-31 14:05:29 +0330

    update

--- portfilio/src/component/cvDownload/CV.jsx
+++ portfilio/src/component/cvDownload/CV.jsx
@@ -1,13 +1,11 @@
-import './CV.css'
+import './CV.css';
+import resumeFile from '/resume.pdf';
 
 function DownloadButton() {
   const handleDownload = () => {
-
-    const fileUrl = '/../.././resume.pdf'; 
-    
     const link = document.createElement('a');
-    link.href = fileUrl;
-    link.download = "/resume.pdf"; 
+    link.href = resumeFile;
+    link.download = "resume.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
